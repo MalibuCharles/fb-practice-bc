00da857dfd63dfe5cb40c93bb2af146bede02160
Berthonise Charles <berthonisecharles@Berthonises-MacBook-Air-2.local>
2022-01-27 11:45:14 -0500

    added firestore

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // import our restaurants
+const { require } = require('yargs');
 const restaurants = require('./restaurants.json');
 
 console.log(restaurants[0].name)
 
-// connect to Firestore
 
+// import a set of tools to talk to firebase and firestore
+const {initializeAp, applicationDefault, cert} = require('firebase-admin/app');
+const {getFirestore, Timestamp, Fieldvalue} = require('firebase-admi/firestore');
+
+
+// connect to Firestore
+const credentials = require('./restaurants.json');
 /// create a collection called "restaurants"
 
 // add each restaurants
